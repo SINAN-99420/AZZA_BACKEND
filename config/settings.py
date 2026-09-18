200386e21d5c5ca790bca9b91b67029096e907ca
SINAN-99420 <sinankp6984@gmail.com>
2026-09-18 16:07:10 +0530

    change

--- config/settings.py
+++ config/settings.py
@@ -219,7 +219,7 @@ EMAIL_BACKEND = (
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173"
+    "https://azza-backend.onrender.com,https://azza-three.vercel.app/"
 ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
@@ -231,7 +231,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "CSRF_TRUSTED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173"
+    "https://azza-backend.onrender.com,https://azza-three.vercel.app/"
 ).split(",")
 
 
